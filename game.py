77d5ef6e3133cfd299ec53f57594f651ba6452c1
Jacob Rogers <jacob.p.r@hotmail.com>
2024-03-29 12:24:30 +1300

    Make precommit happy

--- game.py
+++ game.py
@@ -1,19 +1,22 @@
-import pygame
+# import pygame
+
 
 class Game:
     def __init__(self):
-    
-    
+        pass
+
     def on_loop(self):
-        while(1):
-            # Check inputs 
+        while 1:
+            # Check inputs
             # Update Game
             # Render
-        
+            pass
+
     def check_inputs(self):
-        '''Checks for '''
-    
+        """Checks for"""
+
     def update(self):
-        
+        pass
+
     def render(self):
-        '''Do render pls'''
+        """Do render pls"""
