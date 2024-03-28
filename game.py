01429f70a4f89b132d3bafb4de87fc51e27ed251
MakoTime <ben.vernon.bosley@gmail.com>
2024-03-29 11:42:28 +1300

    Added Gameloop and game.py

---
+++ game.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Game:
+    def __init__(self):
+    
+    
+    def on_loop(self):
+        while(1):
+            # Check inputs 
+            # Update Game
+            # Render
+        
+    def check_inputs(self):
+        '''Checks for '''
+    
+    def update(self):
+        
+    def render(self):
+        
