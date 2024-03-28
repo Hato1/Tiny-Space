90b9525b9d1cc708e6a53e34e7d34bf16dec0853
MakoTime <ben.vernon.bosley@gmail.com>
2024-03-29 12:32:13 +1300

    Pygame window initialisation

--- game.py
+++ game.py
@@ -1,22 +1,48 @@
-# import pygame
+import pygame
+
+# from pygame.locals import *
 
 
 class Game:
     def __init__(self):
+        self._running = True
+        self._display_surface = None
+        self.size = self.weight, self.height = 640, 400
+
+    def on_init(self):
+        pygame.init()
+        self._display_surface = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._running = True
+
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
+
+    def on_render(self):
         pass
 
     def on_loop(self):
-        while 1:
-            # Check inputs
-            # Update Game
-            # Render
-            pass
-
-    def check_inputs(self):
-        """Checks for"""
-
-    def update(self):
         pass
 
-    def render(self):
-        """Do render pls"""
+    def on_cleanup(self):
+        pygame.QUIT
+
+    def check_inputs(self):
+        """Checks for inputs from the user; mouse keys etc"""
+        pass
+
+    def on_excecute(self):
+        if self.on_init() is False:
+            self._running = False
+
+        while self._running:
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.check_inputs()
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+
+
+if __name__ == "__main__":
+    game = Game()
